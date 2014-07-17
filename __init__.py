168772672d0b61a17f333efec55834efd4e993a4
Christopher Su <christophersu9@gmail.com>
2014-07-16 21:16:55 -0700

    init

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+A Python API for the MacRumors Buyer's Guide
+
+@author Christopher Su
+"""
+
+__title__ = 'PyAppleBuyersGuide'
+__author__ = 'Christopher Su'
+__copyright__ = 'Copyright 2014 Christopher Su'
+
